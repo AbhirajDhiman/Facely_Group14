41cab7fbfe51ff5978fd694bc1346fa38884c4cb
Abhiraj Dhiman <abhirajdhiman@outlook.com>
2025-04-12 20:22:37 +0530

    added spideranimation effect to the cursor movement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SpiderAnimation from './page/SpiderAnimation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <div style={{ position: 'relative' }}>
+      <SpiderAnimation />
+      <App />
+    </div>
+  </StrictMode>
 )
