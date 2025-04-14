9eebc1d51dfaf4b2c745904d70c3ac4f5dfdc427
jaskaransingh012 <jaskaransingh_1@outlook.com>
2025-04-14 20:10:28 +0530

    deepface lib installed and setted up

--- ml-services/app/routes.py
+++ ml-services/app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from .services.faceVerify import compare_faces
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def home():
-    return "Hello from Flask backend! 🚀"
+@main.route('/verify', methods=['POST'])
+def verify_route():
+    try:
+        img1 = request.files['img1']
+        img2 = request.files['img2']
+        result = compare_faces(img1, img2)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
