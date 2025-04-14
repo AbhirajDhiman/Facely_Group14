ac56ff26b60ce66d519ed95024a57bc605176999
jaskaransingh012 <jaskaransingh_1@outlook.com>
2025-04-14 08:52:33 +0530

    the backend for authentication has been completed and the server is running without any issue

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-// const express =require ("express");
 import express from 'express';
-import { connectDB } from './DB/connectdb';
+import { configDotenv } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+
+import { connectDB } from './db/connectDB.js';
+
+
+import authRoutes from './routes/auth.routes.js';
+
+configDotenv();
+
+const PORT = process.env.PORT || 5000;
+
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("Hello Wo rld");
-});
-app.listen(3000, ()=> {
+
+
+app.use(express.json());
+app.use(cookieParser()); //allow us to access the cookie 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use("/api/auth", authRoutes) 
+
+
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 3000");
-});
+    console.log('Server running on port 5000')
+}); 
