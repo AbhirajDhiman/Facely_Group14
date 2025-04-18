e52a81e069e148ab0d95400875e1f1daea8efaf4
jaskaransingh012 <jaskaransingh_1@outlook.com>
2025-04-18 09:22:13 +0530

    updated folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import SpiderAnimation from './page/SpiderAnimation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div style={{ position: 'relative' }}>
-      <SpiderAnimation />
       <App />
     </div>
   </StrictMode>
